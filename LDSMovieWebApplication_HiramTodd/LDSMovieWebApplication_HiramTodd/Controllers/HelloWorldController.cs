2964d19c3a015847899f500b50285e8f790f657f
Hiram Todd <htillustrator@gmail.com>
2019-03-12 22:03:11 -0700

    Controller Added 03/12/2019
    HelloWorldController class added.

---
+++ LDSMovieWebApplication_HiramTodd/LDSMovieWebApplication_HiramTodd/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace LDSMovieWebApplication_HiramTodd.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my deafult action...";
+        }
+
+        //
+        //GET: /HelloWorld/Welcome/
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
